6bbc8b091880e4b793dba9651e705322d8ec795c
Anjana Supun <anjanasupun05@gmail.com>
2017-09-10 21:47:28 +0530

    code refactor

--- server/server.js
+++ server/server.js
@@ -1,37 +1,13 @@
-var mongoose = require('mongoose');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
-});
 
 var newTodo = new Todo({
     text: 'Eat Dinner'
 });
 
 newTodo.save().then((result) => {
-    console.log("Saved", result);
-}, (err) => {
-    console.log("Error", err);
-})
-
-var newTodo1 = new Todo({
-    text: 'Fuck Dinner',
-    completed: false,
-    completedAt: 123
-});
-
-newTodo1.save().then((result) => {
     console.log("Saved", result);
 }, (err) => {
     console.log("Error", err);
